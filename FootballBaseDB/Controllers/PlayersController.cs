deb85fc91fc0b1dad562966007d89af7db0672e9
Yuriy Serbin <serbingeorg@hotmail.com>
2019-10-18 13:00:18 +0300

    player get method

--- FootballBaseDB/Controllers/PlayersController.cs
+++ FootballBaseDB/Controllers/PlayersController.cs
@@ -23,8 +23,35 @@ namespace FootballBaseDB.Controllers
             Player player = db.Players.Find(id);
             return player;
         }
+        [HttpPost]
+        public void CreatePlayer([FromBody] Player player)
+        {
+            db.Players.Add(player);
+            db.SaveChanges();
+        }
+
+        [HttpPut]
+        public void EditPlayer(int id, [FromBody] Player player)
+        {
+            if (id == player.Id)
+            {
+                db.Entry(player).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeletePlayer(int id)
+        {
+            Player player = db.Players.Find(id);
+            if (player != null)
+            {
+                db.Players.Remove(player);
+                db.SaveChanges();
+
+            }
+        }
 
 
-      
+
     }
 }
